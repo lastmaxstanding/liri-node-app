afea2bdff98f6ffae0240b746026906a2ea77609
Max <maxferrer@Max.local>
2016-06-10 18:04:49 -0400

    added variables

---
+++ keys.js
@@ -0,0 +1,8 @@
+console.log('this is loaded');
+
+exports.twitterKeys = {
+  consumer_key: '7mQ2O0Xpn1f9l0lVwXVhJlihD',
+  consumer_secret: 'CYx3v1L5p2TwWEfrNrKBNC3lsvXPMQvsecqCn1yYWDdoRbHcpz',
+  access_token_key: '605146260-lryhUTPEnoX53JnPNy4C1VfdyRsrgizFpWHOQntd',
+  access_token_secret: 'ILMfpgvxaCiDEtiAFLG7sHZyBKofplOFWgheeHPgngIjF',
+}
---
+++ liri.js
@@ -0,0 +1,12 @@
+var fs = require('fs');
+var Twitter = require('twitter')
+
+var myTweets = process.argv[2]
+var mySpotify = process.argv[3]
+var myMovie = process.argv[4]
+var myWhat = process.argv[5]
+
+
+
+
+console.log(myTweets);
